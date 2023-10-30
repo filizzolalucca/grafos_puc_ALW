9878d4d9785ebd28a3ae8f23447e483f52008db8
Wemerson-ferr <wemerson-b@hotmail.com>
2023-10-30 15:40:50 -0300

    Subindo funções criadas da matriz de inciencia

--- src/GrafoMatrizAdjacencia.java
+++ src/GrafoMatrizAdjacencia.java
@@ -1,41 +1,76 @@
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class GrafoMatrizAdjacencia implements IGrafo{
 
+    private int[][] matriz;
+    private boolean direcionado;
+    private int[] pesosVertices;
+    private String[] rotulosVertices;
+    // private int[] pesosArestas;
+    private Map<String, String> rotulosArestas = new HashMap<>();
+
+    
+
+    public GrafoMatrizAdjacencia(int tamanho, boolean direcionado) {
+        matriz = new int[tamanho][tamanho];
+        this.direcionado = direcionado;
+        pesosVertices = new int[tamanho];
+        rotulosVertices = new String[tamanho];
+    }
+
     @Override
     public void inserirAresta(int verticeOrigem, int verticeDestino) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'inserirAresta'");
+        if(verticeOrigem >= 0 && verticeDestino >= 0 && verticeOrigem < matriz.length && verticeDestino < matriz.length ){
+            matriz[verticeOrigem][verticeDestino] = 1;
+        }
     }
 
     @Override
     public void removerAresta(int verticeOrigem, int verticeDestino) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'removerAresta'");
+        if(verticeOrigem >= 0 && verticeDestino >= 0 && verticeOrigem < matriz.length && verticeDestino < matriz.length){
+            matriz[verticeOrigem][verticeDestino] = 0;
+        }
     }
 
     @Override
     public void ponderarVertice(int vertice, int peso) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'ponderarVertice'");
+        if(vertice >= 0 && vertice < pesosVertices.length){
+            pesosVertices[vertice] = peso;
+        }
     }
 
     @Override
     public void rotularVertice(int vertice, String rotulo) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'rotularVertice'");
+        if(vertice >= 0 && vertice < rotulosVertices.length){
+            rotulosVertices[vertice] = rotulo;
+        }
     }
 
     @Override
     public void ponderarAresta(int verticeOrigem, int verticeDestino, int peso) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'ponderarAresta'");
+        if(verticeOrigem >= 0 && verticeDestino >= 0 && verticeOrigem < matriz.length && verticeDestino < matriz.length){
+            matriz[verticeOrigem][verticeDestino] = peso;
+        }
     }
 
     @Override
     public void rotularAresta(int verticeOrigem, int verticeDestino, String rotulo) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'rotularAresta'");
+        if(verificaExistenciaArestas(verticeOrigem, verticeDestino)){
+            if(verticeOrigem >= 0 && verticeDestino >= 0 && verticeOrigem < matriz.length && verticeDestino < matriz.length){
+                String chave = verticeOrigem + "-" + verticeDestino;
+                rotulosArestas.put(chave, rotulo);
+            }
+        }
+        else{
+            System.out.println("Aresta inexistente ou inválida");
+        }
+    }
+
+     @Override
+    public boolean verificaExistenciaArestas(int verticeOrigem, int verticeDestino) {
+         return matriz[verticeOrigem][verticeDestino] != 0;
     }
 
     @Override
@@ -57,27 +92,37 @@ public class GrafoMatrizAdjacencia implements IGrafo{
     }
 
     @Override
-    public boolean verificaExistenciaArestas() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'verificaExistenciaArestas'");
-    }
-
-    @Override
     public int getNumeroVertices() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getNumeroVertices'");
+        return matriz.length;
     }
 
     @Override
     public int getNumeroArestas() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getNumeroArestas'");
+        int numArestas = 0;
+        int numVertices = matriz.length;
+
+        for (int i = 0; i < numVertices; i++) {
+            for (int j = 0; j < numVertices; j++) {
+                if (matriz[i][j] != 0) {
+                    numArestas++;
+                }
+            }
+    }
+        if(direcionado) return numArestas/2;
+
+        else return numArestas;
     }
 
     @Override
     public boolean isGrafoVazio() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'isGrafoVazio'");
+        for(int i = 0; i< matriz.length; i++){
+            for(int j = 0; j< matriz[0].length; j++){
+                if(matriz[i][j] != 0){
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
     @Override
@@ -102,6 +147,66 @@ public class GrafoMatrizAdjacencia implements IGrafo{
     public int[][] importarGrafo() {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'importarGrafo'");
+    }
+
+    @Override
+    public void dijkstra() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'dijkstra'");
+    }
+
+    @Override
+    public void bellmanFord() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'bellmanFord'");
+    }
+
+    @Override
+    public void floydWarshall() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'floydWarshall'");
+    }
+
+    @Override
+    public int dijkstraMenorDistanciaUmParaTodos() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'dijkstraMenorDistanciaUmParaTodos'");
+    }
+
+    @Override
+    public int dijkstraMenorDistanciaTodosParaTodos() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'dijkstraMenorDistanciaTodosParaTodos'");
+    }
+
+    @Override
+    public int bellmanFordMenorDistanciaUmParaTodos() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'bellmanFordMenorDistanciaUmParaTodos'");
+    }
+
+    @Override
+    public int bellmanFordMenorDistanciaTodosParaTodos() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'bellmanFordMenorDistanciaTodosParaTodos'");
+    }
+
+    @Override
+    public int floydWarshallMenorDistanciaUmParaTodos() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'floydWarshallMenorDistanciaUmParaTodos'");
+    }
+
+    @Override
+    public int floydWarshallMenorDistanciaTodosParaTodos() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'floydWarshallMenorDistanciaTodosParaTodos'");
+    }
+
+    @Override
+    public void AEstrela() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'AEstrela'");
     }
     
 }
